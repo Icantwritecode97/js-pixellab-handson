e844f1dde8f957cd74fdb9c61872e3abea19d68a
Icantwritecode97 <96843873+Icantwritecode97@users.noreply.github.com>
2022-05-31 22:01:14 +0300

    clasa 31.05 js

---
+++ M01S04/ex06/app.js
@@ -0,0 +1,34 @@
+var person = {
+  name: 'Marius',
+  surname: 'Dan',
+  age: 35,
+  petOwner: false,
+
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'C++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larrysin',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
